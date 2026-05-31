4e450ed40c903363f2dcc656af90dcf582bdf1e8
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-05-31 11:32:14 -0500

    done

--- Spiral Matrix II.py
+++ Spiral Matrix II.py
@@ -1,61 +1,100 @@
-from typing import List
+from collections import deque
 
+"""
+What I learned:
+DO NOT USE INSERT ON LISTS! Use a lookup like a dictionary to replace items in a matrix or list. Also, python has a queue data structure.
+"""
+
+"""
 def generateMatrix(n: int):
-    temp_matrix = [[0 for j in range(n)] for i in range(n)]
-    numbers = [n for n in range(1, (n**2)+1, 1)]
+    temp_matrix = [[0 for _ in range(n)] for _ in range(n)]
+    numbers = deque(range(1, n*n + 1))
+
     row_index = 0
     col_index = 0
     sprial_count = 0
-    
-    while True:
-        try:
-            if col_index < n - sprial_count and row_index == 0 + sprial_count:
-                print("GOING RIGHT")
-                number = numbers.pop(0)
-                
-                temp_matrix[row_index].pop(col_index)
-                
-                temp_matrix[row_index].insert(col_index, number)
-                col_index += 1
-            
-            elif col_index == n - sprial_count and row_index < n - sprial_count:
-                print("GOING DOWN")
-                row_index += 1
-                if row_index == n - sprial_count:
-                    continue
-                number = numbers.pop(0)
-                temp_matrix[row_index].pop(col_index  - 1)
-                temp_matrix[row_index].insert(col_index  - 1, number)
 
-            elif row_index == n - sprial_count and col_index > 1 + sprial_count:
-                print("GOING LEFT")
-                col_index -= 1
-                number = numbers.pop(0)
-                temp_matrix[row_index-1].pop(col_index - 1)
-                temp_matrix[row_index-1].insert(col_index -  1, number)
-            
-            elif col_index == sprial_count + 1 and row_index > sprial_count-1:
-                print("GOING UP")
-                row_index -= 1
-                if row_index == 1 + sprial_count:
-                    sprial_count += 1
-                    row_index = sprial_count
-                    col_index = sprial_count
-                    continue
+    while numbers:
 
-                number = numbers.pop(0)
-                temp_matrix[row_index-1].pop(col_index - 1)
-                temp_matrix[row_index-1].insert(col_index  - 1, number)
-        except Exception:
-            break
-                
+        # GO RIGHT
+        if col_index < n - sprial_count and row_index == sprial_count:
+            temp_matrix[row_index][col_index] = numbers.popleft()
+            col_index += 1
+            continue
+
+        # GO DOWN
+        if col_index == n - sprial_count and row_index < n - sprial_count:
+            row_index += 1
+            if row_index < n - sprial_count:
+                temp_matrix[row_index][col_index - 1] = numbers.popleft()
+            continue
+
+        # GO LEFT
+        if row_index == n - sprial_count and col_index > sprial_count + 1:
+            col_index -= 1
+            temp_matrix[row_index - 1][col_index - 1] = numbers.popleft()
+            continue
+
+        # GO UP
+        if col_index == sprial_count + 1 and row_index > sprial_count:
+            row_index -= 1
+
+            # reached top of this layer → move inward
+            if row_index == sprial_count + 1:
+                sprial_count += 1
+                row_index = sprial_count
+                col_index = sprial_count
+                continue
+
+            temp_matrix[row_index - 1][col_index - 1] = numbers.popleft()
+            continue
 
     return temp_matrix
+"""
 
-        
-    
 
-    
+def generateMatrix(n: int):
+    matrix = [[0] * n for _ in range(n)]
+    nums = deque(range(1, n * n + 1))
+
+    r = c = 0
+    layer = 0
+
+    while nums:
+
+        # RIGHT
+        while c < n - layer and nums:
+            matrix[r][c] = nums.popleft()
+            c += 1
+        c -= 1
+        r += 1
+
+        # DOWN
+        while r < n - layer and nums:
+            matrix[r][c] = nums.popleft()
+            r += 1
+        r -= 1
+        c -= 1
+
+        # LEFT
+        while c >= layer and nums:
+            matrix[r][c] = nums.popleft()
+            c -= 1
+        c += 1
+        r -= 1
+
+        # UP
+        while r > layer and nums:
+            matrix[r][c] = nums.popleft()
+            r -= 1
+
+        # move to next inner layer
+        layer += 1
+        r = layer
+        c = layer
+
+    return matrix
+
 
 if __name__ == "__main__":
-    print(generateMatrix(5))
+    print(generateMatrix(6))
